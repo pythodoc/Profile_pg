347bbe7cda2120c26302e603d20cdc2abb15c37f
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2025-07-04 23:50:41 +0530

    Update Gaurav_N_Digital_CV.py

--- Gaurav_N_Digital_CV.py
+++ Gaurav_N_Digital_CV.py
@@ -6,13 +6,12 @@ import time as tmod
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-#current_dir=r"https://github.com/pythodoc/Profile_pg/blob/main/"
+
 css_file = current_dir / "main.css"
-#css_file=current_dir+r"\styles\main.css"
+
 resume_file = current_dir / "CV.pdf"
-#resume_file=current_dir+r"\assets\CV.pdf"
+
 profile_pic = current_dir / "gaurav profile_pic1.PNG"
-#profile_pic=current_dir+r"\assets\gaurav profile_pic1.PNG"
 
 
 # --- GENERAL SETTINGS ---
