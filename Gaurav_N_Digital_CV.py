5bd597bd8b96e014e3c93c2a459a9af0f31a804d
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2025-09-08 18:52:17 +0530

    Update Gaurav_N_Digital_CV.py
    update of the overall expirence

--- Gaurav_N_Digital_CV.py
+++ Gaurav_N_Digital_CV.py
@@ -82,7 +82,7 @@ st.write('\n')
 st.subheader("Experience & Qualifications")
 st.write(
     """
-- ✔️ 1 Years experience extracting actionable insights from data
+- ✔️ 1.8 Years experience extracting actionable insights from data
 - ✔️ Strong hands on experience and knowledge in Python and Excel
 - ✔️ Good understanding of statistical principles and their respective applications
 - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
